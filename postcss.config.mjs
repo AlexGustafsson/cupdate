835385d638fca5d4d48a69a5400473cf38ebb076
Alex Gustafsson <alex-gustafsson@live.se>
2024-10-06 11:47:40 +0200

    Improve markdown rendering
    Some image descriptions contains image tags with specifically set
    heights. This did not work well with tailwind's defaults.
    
    Remove these defaults and make the layout look more like on Docker
    Hub, for example.

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,6 +1,22 @@
 import autoprefixer from 'autoprefixer'
 import tailwindcss from 'tailwindcss'
 
+// Markdown can have imgs using the "height" attribute. This is overridden by
+// tailwind's defaults as it has a higher specificity. The rule is unused by us
+// and can be introduced manually when needed instead.
+// SEE: https://github.com/tailwindlabs/tailwindcss/pull/7742#issuecomment-1061332148
+const removeTailwindImgAutoHeight = (css) => {
+  css.walkRules(function (rule) {
+    if (rule.selector === 'img,\nvideo') {
+      rule.walkDecls(function (decl) {
+        if (['height'].indexOf(decl.prop) !== -1) {
+          decl.remove()
+        }
+      })
+    }
+  })
+}
+
 export default {
-  plugins: [tailwindcss, autoprefixer],
+  plugins: [tailwindcss, autoprefixer, removeTailwindImgAutoHeight],
 }
