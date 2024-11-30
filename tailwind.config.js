b22931a2914e54618bad01a9476f0e71ceeec177
Alex Gustafsson <alex-gustafsson@live.se>
2024-11-30 14:32:57 +0100

    Add fluent box shadow

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@ module.exports = {
   mode: 'jit',
   content: ['./web/**/*.{html,tsx}'],
   darkMode: 'media',
+  theme: {
+    extend: {
+      boxShadow: {
+        around: '0 0 2px rgba(0, 0, 0, 0.22), 0 4px 8px rgba(0, 0, 0, 0.28)',
+      },
+    },
+  },
   variants: {
     extend: {},
   },
