45bfca9f70f5b19e7208491af6c466811a306d76
Alex Gustafsson <alex-gustafsson@live.se>
2024-12-14 22:18:08 +0100

    Create a service-worker-based demo mode

--- web/main.tsx
+++ web/main.tsx
@@ -7,11 +7,42 @@ import './main.css'
 
 const root = document.getElementById('root')
 if (root) {
-  createRoot(root).render(
-    <React.StrictMode>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </React.StrictMode>
-  )
+  if (import.meta.env['VITE_DEMO_MODE'] === 'true') {
+    console.log('Starting Cupdate in demo mode')
+    navigator.serviceWorker
+      .getRegistrations()
+      .then((registrations) =>
+        Promise.all(registrations.map((x) => x.unregister()))
+      )
+      .then(() =>
+        navigator.serviceWorker.register('/demo-service-worker.js', {
+          scope: '/',
+        })
+      )
+      .then(() => {
+        createRoot(root).render(
+          <React.StrictMode>
+            <BrowserRouter>
+              <App />
+            </BrowserRouter>
+          </React.StrictMode>
+        )
+      })
+      .catch((error) => {
+        createRoot(root).render(
+          <React.StrictMode>
+            <p>Service workers are required for the demo to work.</p>
+            <pre>{error}</pre>
+          </React.StrictMode>
+        )
+      })
+  } else {
+    createRoot(root).render(
+      <React.StrictMode>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </React.StrictMode>
+    )
+  }
 }
