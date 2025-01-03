4a869197b96969cdec7f4587efd5b8aa172ad928
Alex Gustafsson <alex-gustafsson@live.se>
2025-01-03 17:10:00 +0100

    Fix tag selection on small screens
    When no images are added (or all are filtered out), the tag selection
    would be cut off due to the fix in 8f7278f. The root cause has now been
    found to be the tooltips which have an absolute component hovering above
    the tooltip's "parent".
    
    Work around the root cause by only rendering the tooltip when hovered,
    rather than controlling the visibility.
    
    A proper solution would require additional features not yet available in
    all browsers. See #23.

--- web/App.tsx
+++ web/App.tsx
@@ -56,7 +56,7 @@ export function App(): JSX.Element {
           />
         )}
       </div>
-      <main className="pt-[64px] overflow-hidden">
+      <main className="pt-[64px]">
         <Routes>
           <Route path="/" element={<Dashboard />} />
           <Route path="/image" element={<ImagePage />} />
