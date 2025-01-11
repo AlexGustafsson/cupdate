e3a18cfebd7efcc28d9f20c85b37b92eec184c95
Alex Gustafsson <alex-gustafsson@live.se>
2025-01-11 11:32:47 +0100

    Add Docker compose update script example

--- docs/cookbook/README.md
+++ docs/cookbook/README.md
@@ -2,8 +2,8 @@
 
 ## Update kustomizations using Cupdate's API
 
-If you're using kustomizations to write and deploy your Kubernetes APIs you can
-use Cupdate's API, curl, [yq](https://github.com/mikefarah/yq) and
+If you're using kustomizations to write and deploy your Kubernetes services you
+can use Cupdate's API, curl, [yq](https://github.com/mikefarah/yq) and
 [jq](https://github.com/jqlang/jq) to update the manifests.
 
 Let's say you have a manifest like the one below.
@@ -28,6 +28,29 @@ resources:
 You can then use a script like
 [update-kustomizations.sh](update-kustomizations.sh) to update such manifests
 by using Cupdate's APIs.
+
+The key here is to use yq to identify all image overrides and then curl to query
+Cupdate. Finally, jq is used to parse the response and yq again to change the
+manifest.
+
+## Update compose files using Cupdate's API
+
+If you're using Docker compose files to write and deploy your Docker services
+you can use Cupdate's API, curl, [yq](https://github.com/mikefarah/yq) and
+[jq](https://github.com/jqlang/jq) to update the manifests.
+
+Let's say you have a compose file like the one below.
+
+```yaml
+# compose.yaml
+services:
+  cupdate:
+    image: ghcr.io/alexgustafsson/cupdate:0.14.1
+```
+
+You can then use a script like
+[update-compose-files.sh](update-compose-files.sh) to update such files by using
+Cupdate's APIs.
 
 The key here is to use yq to identify all image overrides and then curl to query
 Cupdate. Finally, jq is used to parse the response and yq again to change the
---
+++ docs/cookbook/update-compose-files.sh
@@ -0,0 +1,73 @@
+#!/usr/bin/env bash
+
+# This script will go through all compose files, looking for images defined like
+# so:
+# services:
+#   cupdate:
+#     image: ghcr.io/alexgustafsson/cupdate:0.14.1
+#
+# For all such images, the script will perform a lookup towards Cupdate's API to
+# find the latest image and update the manifest in-place if the user whishes to
+# do so.
+
+# CUPDATE_API must be set to the URL where your Cupdate instance is reachable
+CUPDATE_API="https://cupdate.home.local/api/v1/image"
+# COMPOSE_FILE_NAME is the name of compose files. All files by this name
+# in current directory will be checked
+COMPOSE_FILE_NAME="compose.yaml"
+
+function confirm() {
+  while true; do
+    echo -n "$1 [Y/n] "
+    read -rsn 1 result </dev/tty
+    echo
+    case "$result" in
+    'y' | 'Y' | '')
+      return 0
+      ;;
+    'n' | 'N')
+      return 1
+      ;;
+    *)
+      continue
+      ;;
+    esac
+  done
+}
+
+# For each compose file found in the current directory
+while read -r composeFile; do
+  # For each image reference tag replacement found in the compose file, such as
+  # services:
+  #   cupdate:
+  #     image: ghcr.io/alexgustafsson/cupdate:0.14.1
+  while read -r reference; do
+    if [[ -z "$reference" ]]; then
+      continue
+    fi
+
+    # Use the Cupdate API to find the latest reference
+    latestReference="$(curl --silent --get "$CUPDATE_API" --data-urlencode "reference=$reference" | jq -rc .latestReference 2>/dev/null)"
+    if [[ ! $! -eq 0 ]] || [[ -z "$latestReference" ]] || [[ "$latestReference" = "$reference" ]]; then
+      continue
+    fi
+
+    # Print a header and ask for confirmation
+    echo "$composeFile"
+    echo -e "\e[31m$reference\e[0m -> \e[32m$latestReference\e[0m"
+    confirm "Update?"
+
+    # If update is confirmed
+    if [[ $? -eq 0 ]]; then
+      # Create a diff using yq as yq doesn't support keeping whitespace or
+      # comments around
+      diff="$(diff -u --ignore-blank-lines -L "$composeFile" -L "$composeFile" <(grep -v '^$' "$composeFile") <(yq ".services[] |= select(.image == \"$reference\").image = \"$latestReference\"" "$composeFile"))"
+      # Print and apply the diff
+      echo "$diff"
+      echo "$diff" | patch --silent --unified --posix --fuzz 3 "$composeFile" -i -
+    fi
+
+    # Separate runs by a newline
+    echo
+  done <<<"$(yq '.services[] | select(.image|type == "!!str") | .image' "$composeFile" | grep -v '^$')"
+done <<<"$(find . -type f -name "$COMPOSE_FILE_NAME")"
