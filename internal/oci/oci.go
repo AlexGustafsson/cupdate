bd8fe542416e006d35279ea28cfca2cfd0609b1b
Alex Gustafsson <alex-gustafsson@live.se>
2025-01-22 17:51:14 +0100

    Add package docs

---
+++ internal/oci/oci.go
@@ -0,0 +1,9 @@
+// Package oci implemens utilities for interacting with Open Container
+// Initiative image reference, annotations, distribution (registry) APIs and
+// more.
+// SEE: https://github.com/opencontainers/distribution-spec/blob/main/spec.md
+// SEE: https://github.com/opencontainers/image-spec/blob/main/annotations.md
+// SEE: https://github.com/opencontainers/image-spec/blob/main/spec.md
+// SEE: https://github.com/distribution/reference
+// SEE: https://github.com/opencontainers/artifacts
+package oci
