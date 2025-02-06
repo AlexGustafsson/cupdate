ba759163c1f0861e3ee7a9e343e06e6981a5d3d6
Alex Gustafsson <alex-gustafsson@live.se>
2025-02-06 21:33:30 +0100

    Add metrics for worker queue

--- internal/worker/queue.go
+++ internal/worker/queue.go
@@ -5,6 +5,8 @@ import (
 	"slices"
 	"sync"
 	"time"
+
+	"github.com/prometheus/client_golang/prometheus"
 )
 
 type Queue[T comparable] struct {
@@ -12,6 +14,9 @@ type Queue[T comparable] struct {
 	backlog []T
 	tokens  int
 	closed  bool
+
+	burstGauge  prometheus.Gauge
+	lengthGauge prometheus.Gauge
 }
 
 func NewQueue[T comparable](burst int, tick time.Duration) *Queue[T] {
@@ -19,6 +24,17 @@ func NewQueue[T comparable](burst int, tick time.Duration) *Queue[T] {
 		cond:    sync.NewCond(&sync.Mutex{}),
 		backlog: make([]T, 0),
 		tokens:  burst,
+
+		burstGauge: prometheus.NewGauge(prometheus.GaugeOpts{
+			Namespace: "cupdate",
+			Subsystem: "worker",
+			Name:      "available_burst",
+		}),
+		lengthGauge: prometheus.NewGauge(prometheus.GaugeOpts{
+			Namespace: "cupdate",
+			Subsystem: "worker",
+			Name:      "queue_length",
+		}),
 	}
 
 	// Fill the available backlog over time
@@ -34,6 +50,7 @@ func NewQueue[T comparable](burst int, tick time.Duration) *Queue[T] {
 				q.cond.L.Unlock()
 
 				if changed {
+					q.burstGauge.Set(float64(tokens))
 					q.cond.Broadcast()
 				}
 			}
@@ -58,6 +75,7 @@ func (q *Queue[T]) Push(items ...T) {
 		}
 	}
 
+	q.lengthGauge.Set(float64(len(q.backlog)))
 	q.cond.Broadcast()
 }
 
@@ -70,6 +88,8 @@ func (q *Queue[T]) Pull() iter.Seq[T] {
 			// Try to pick an item from the queue
 			if len(q.backlog) > 0 && q.tokens > 0 {
 				q.tokens--
+				q.burstGauge.Set(float64(q.tokens))
+				q.lengthGauge.Dec()
 				ok := yield(q.backlog[0])
 				q.backlog = q.backlog[1:]
 				if !ok {
@@ -89,10 +109,25 @@ func (q *Queue[T]) Len() int {
 	return len(q.backlog)
 }
 
+func (q *Queue[T]) AvailableBurst() int {
+	return q.tokens
+}
+
 func (q *Queue[T]) Close() {
 	q.cond.L.Lock()
 	defer q.cond.L.Unlock()
 	q.closed = true
 	q.backlog = []T{}
 	q.cond.Broadcast()
+}
+
+// Collect implements [prometheus.Collector].
+func (w *Queue[T]) Collect(ch chan<- prometheus.Metric) {
+	w.burstGauge.Collect(ch)
+	w.lengthGauge.Collect(ch)
+}
+
+// Describe implements [prometheus.Collector].
+func (w *Queue[T]) Describe(descs chan<- *prometheus.Desc) {
+	prometheus.DescribeByCollect(w, descs)
 }
