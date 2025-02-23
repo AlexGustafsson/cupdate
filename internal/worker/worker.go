AlexGustafsson/cupdate

commit 1dbe24c028ac94d41e3718c8aaf1c119c940f2a4

Sort digest diffs as if they were patches
Cupdate can sort images by their semver "bump". For images that don't
support semver, using digests, for example, store the "bump" as being a
patch.

internal/worker/worker.go
@@ -106,23 +106,15 @@ func (w *Worker) ProcessRawImage(ctx context.Context, reference oci.Reference) e
 
 	versionDiffSortable := semver.PackInt64(nil)
 
-	// TODO: This should be removed, right?
-	// Fixed tags are handled now, latest should work?
-	// Should all fixed tags' diffSortable be set to 0?
-	// // If no new version was defined and the current version is using the "latest"
-	// // convention, the latest available reference is the current reference
-	// if reference.Version() == "latest" {
-	// 	r := reference
-	// 	data.LatestReference = &r
-	// 	versionDiffSortable = 0
-	// }
-
 	// Add some basic tags
 	if data.LatestReference != nil {
 		if data.ImageReference.String() == data.LatestReference.String() {
 			data.InsertTag("up-to-date")
 		} else {
 			data.InsertTag("outdated")
+			// We know that the image is outdated, default to assuming the update is a
+			// patch to handle cases like where digests diff but not the tag itself
+			versionDiffSortable = semver.PackedSingleDigitPatchDiff
 		}
 
 		// Add tags based on version diff

internal/semver/utils.go
@@ -101,3 +101,5 @@ func PackInt64(version *Version) uint64 {
 
 	return packed
 }
+
+var PackedSingleDigitPatchDiff = PackInt64(&Version{Release: []int{1, 0, 1}}) - PackInt64(&Version{Release: []int{1, 0, 0}})

internal/semver/utils_test.go
@@ -238,6 +238,13 @@ func TestLatestVersionOnSameTrack(t *testing.T) {
 }
 
 func TestPackInt64(t *testing.T) {
+	// Assert that the zero value is a non-zero value (to differ from an unset
+	// value)
+	assert.Equal(t, uint64(1), PackInt64(nil))
+
+	// Assert that 1 patch diff is 1, skipping the zero value
+	assert.Equal(t, uint64(2), PackedSingleDigitPatchDiff)
+
 	expected := []string{
 		"0",
 		"0.0",

commit 69eebc5562a99a74c29a1fac2969b9fb9444f318

Fix sort of semver tags with digest diff
Fix the diff sortable value of images with semantic tags that are equal,
but with digests that differ. The bug was introduced in
1dbe24c028ac94d41e3718c8aaf1c119c940f2a4.

internal/worker/worker.go
@@ -138,16 +138,14 @@ func (w *Worker) ProcessRawImage(ctx context.Context, reference oci.Reference) e
 			versionDiffSortable = semver.PackedSingleDigitPatchDiff
 		}
 
-		// Add tags based on version diff
 		currentVersion, currentVersionErr := semver.ParseVersion(data.ImageReference.Version())
 		newVersion, newVersionErr := semver.ParseVersion(data.LatestReference.Version())
 		if currentVersion != nil && currentVersionErr == nil && newVersion != nil && newVersionErr == nil {
 			diff := currentVersion.Diff(newVersion)
 			if diff != "" {
 				data.InsertTag(diff)
+				versionDiffSortable = semver.PackInt64(newVersion) - semver.PackInt64(currentVersion)
 			}
-
-			versionDiffSortable = semver.PackInt64(newVersion) - semver.PackInt64(currentVersion)
 		}
 	}
 