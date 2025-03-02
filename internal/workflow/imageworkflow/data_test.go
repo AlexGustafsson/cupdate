10996769bb713f1ce68e1fd6c8a9d2686d5d5237
Alex Gustafsson <alex-gustafsson@live.se>
2025-03-02 16:48:31 +0100

    Add unit tests for data utilities

---
+++ internal/workflow/imageworkflow/data_test.go
@@ -0,0 +1,100 @@
+package imageworkflow
+
+import (
+	"testing"
+
+	"github.com/AlexGustafsson/cupdate/internal/models"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDataInsertTag(t *testing.T) {
+	data := Data{
+		Tags: make([]string, 0),
+	}
+
+	data.InsertTag("tag")
+	assert.Equal(t, []string{"tag"}, data.Tags)
+
+	// No duplicates
+	data.InsertTag("tag")
+	assert.Equal(t, []string{"tag"}, data.Tags)
+}
+
+func TestDataInsertLink(t *testing.T) {
+	data := Data{
+		Links: make([]models.ImageLink, 0),
+	}
+
+	data.InsertLink(models.ImageLink{
+		Type: "generic",
+		URL:  "https://example.com",
+	})
+	data.InsertLink(models.ImageLink{
+		Type: "svc",
+		URL:  "https://example.com/git",
+	})
+	assert.Equal(t, []models.ImageLink{
+		{
+			Type: "generic",
+			URL:  "https://example.com",
+		},
+		{
+			Type: "svc",
+			URL:  "https://example.com/git",
+		},
+	}, data.Links)
+
+	// No duplicates
+	data.InsertLink(models.ImageLink{
+		Type: "generic",
+		URL:  "https://example.com",
+	})
+	assert.Equal(t, []models.ImageLink{
+		{
+			Type: "generic",
+			URL:  "https://example.com",
+		},
+		{
+			Type: "svc",
+			URL:  "https://example.com/git",
+		},
+	}, data.Links)
+}
+
+func TestDataInsertVulnerabilities(t *testing.T) {
+	data := Data{
+		Vulnerabilities: make([]models.ImageVulnerability, 0),
+	}
+
+	data.InsertVulnerability(models.ImageVulnerability{
+		Severity:  "critical",
+		Authority: "example",
+		Links:     []string{},
+	})
+	assert.Equal(t, []models.ImageVulnerability{
+		{
+			Severity:  "critical",
+			Authority: "example",
+			Links:     []string{},
+		},
+	}, data.Vulnerabilities)
+
+	// Duplicates possible, currently no way to uniquely identify them
+	data.InsertVulnerability(models.ImageVulnerability{
+		Severity:  "critical",
+		Authority: "example",
+		Links:     []string{},
+	})
+	assert.Equal(t, []models.ImageVulnerability{
+		{
+			Severity:  "critical",
+			Authority: "example",
+			Links:     []string{},
+		},
+		{
+			Severity:  "critical",
+			Authority: "example",
+			Links:     []string{},
+		},
+	}, data.Vulnerabilities)
+}
