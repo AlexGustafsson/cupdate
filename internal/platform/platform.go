f14d9dd1d66edbb0cacfebfef73e5ff9f53e6c3c
Alex Gustafsson <alex-gustafsson@live.se>
2025-11-21 16:58:48 +0100

    Move platform labels to separate file, add tests

--- internal/platform/platform.go
+++ internal/platform/platform.go
@@ -5,14 +5,11 @@ import (
 	"errors"
 	"fmt"
 	"log/slog"
-	"maps"
-	"strings"
 	"sync"
 	"time"
 
 	"github.com/AlexGustafsson/cupdate/internal/graph"
 	"github.com/AlexGustafsson/cupdate/internal/oci"
-	"github.com/AlexGustafsson/cupdate/internal/semver"
 )
 
 // Node is a platform resource represented as a graph node.
@@ -25,74 +22,6 @@ type Node interface {
 	Labels() Labels
 	// InternalLabels returns labels set by Cupdate.
 	InternalLabels() InternalLabels
-}
-
-// Labels holds labels / annotations found by platform implementations, which
-// map to things like Docker labels or Kubernetes resource annotations.
-type Labels map[string]string
-
-// Ignore returns true if the Cupdate ignore label is set to true.
-func (l Labels) Ignore() bool {
-	v, _ := l.oneOf("config.cupdate/ignore", "cupdate.config.ignore")
-	return v == "true"
-}
-
-// Pin returns true if the Cupdate pin label is set to true.
-func (l Labels) Pin() bool {
-	v, _ := l.oneOf("config.cupdate/pin", "cupdate.config.pin")
-	return v == "true"
-}
-
-// StayOnCurrentMajor returns true if the Cupdate stay-on-current-major label is
-// set to true.
-func (l Labels) StayOnCurrentMajor() bool {
-	v, _ := l.oneOf("config.cupdate/stay-on-current-major", "cupdate.config.stay-on-current-major")
-	return v == "true"
-}
-
-// StayBelow returns the a semver if the Cupdate stay-below label is set.
-// Returns an error if the value is set, but invalid.
-func (l Labels) StayBelow() (*semver.Version, error) {
-	v, ok := l.oneOf("config.cupdate/stay-below", "cupdate.config.stay-below")
-	if !ok {
-		return nil, nil
-	}
-
-	version, err := semver.ParseVersion(v)
-	if err != nil {
-		return nil, fmt.Errorf("label: %w", err)
-	}
-
-	if version.Prerelease != "" || version.Suffix != "" {
-		return nil, fmt.Errorf("label: invalid semver - only specify release")
-	}
-
-	return version, nil
-}
-
-func (l Labels) oneOf(keys ...string) (string, bool) {
-	if l == nil {
-		return "", false
-	}
-
-	for _, k := range keys {
-		if v, ok := l[k]; ok {
-			return v, ok
-		}
-	}
-
-	return "", false
-}
-
-// RemoveUnsupported removes unsupported labels.
-func (l Labels) RemoveUnsupported() Labels {
-	clone := maps.Clone(l)
-	for k := range l {
-		if !strings.HasPrefix(k, "config.cupdate/") && !strings.HasPrefix(k, "cupdate.config.") {
-			delete(clone, k)
-		}
-	}
-	return clone
 }
 
 const (
