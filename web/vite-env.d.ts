5dfdc47a0467e41535071c78d60e344e13337743
Alex Gustafsson <alex-gustafsson@live.se>
2026-07-11 10:16:36 +0200

    Support configuring brand at build time
    Centralize values used in the frontend for branding, making them
    configurable when building.
    
    Document frontend development server, build time args.
    
    Solves: #610

--- web/vite-env.d.ts
+++ web/vite-env.d.ts
@@ -9,6 +9,11 @@ interface ImportMetaEnv {
   readonly VITE_DEMO_MODE?: string
   readonly VITE_CUPDATE_VERSION?: string
   readonly VITE_BASE_PATH?: string
+
+  /** The product name (such as "Cupdate"). */
+  readonly VITE_BRANDING_NAME: string
+  /** The official OCI image reference (such as "github.com/alexgustafsson/cupdate"). */
+  readonly VITE_BRANDING_OCI_REFERENCE: string
 }
 
 interface ImportMeta {
